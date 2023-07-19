a818ab7fd86971d0a8546d0963348dd9bb405663
denuoweb <jaron@rosenau.info>
2023-07-19 03:45:51 -0700

    update

--- src/App.js
+++ src/App.js
@@ -32,10 +32,10 @@ const App = () => {
   }, []);
 
   const fetchDataFromBlockchain = async () => {
-    if (window.ethereum) {
-      // await window.ethereum.send('eth_requestAccounts');
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      window.web3 = new Web3(window.ethereum);
+    if (window.altmasq) {
+      // await window.altmasq.send('eth_requestAccounts');
+      await window.altmasq.request({ method: 'eth_requestAccounts' });
+      window.web3 = new Web3(window.altmasq);
 
       //connecting to metamask
       let web3 = window.web3;
